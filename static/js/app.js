b771ed921904b05be3eaf2a62f4921eed0a22a32
Valentina Herrero <60630391+valentinaherrero@users.noreply.github.com>
2020-07-24 12:33:30 -0400

    updated js

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,24 @@
 
-    d3.select("#exampleFormControlSelect2").addEventListener("change", populateTable(this.value))
+    // making table from mood + song data 
+    d3.select("#exampleFormControlSelect2").addEventListener("change", populateTable(this.val))
     
+    function DropDown(mood) {
+      var pannel = d3.select("#exampleFormControlSelect2")
+
+      fetch(`https://spotifymoodml.herokuapp.com/mood/${mood}`).then(data=>data.json()).then(d=>{
+          console.log(data)
+          d.results.forEach(r=>{
+          o = document.createElement("option")
+          o.text = r
+          pannel.add(o)
+})
+
     function populateTable(song) {
-    var pannel = d3.select("#table")
-    // song_name = []
-    // url = []
-    // mood = []
-    // cluster_name = []
+    var pannel = d3.select("#myInput")
   ​
-    fetch(`https://spotifymoods.herokuapp.com/similar/${song}`).then(data=>data.json()).then(d=>{
-    d.results.forEach(r=>{
+    fetch(`https://spotifymoodml.herokuapp.com/similar/${song}`).then(data=>data.json()).then(d=>{
+        console.log(data)
+        d.results.forEach(r=>{
         const row = pannel.append("tr");
 
         // Loop through each field in the dataRow and add
@@ -23,13 +32,34 @@
     })
     }
 
-    function DropDown(mood) {
-        var pannel = d3.select("#dropdown")
-
-        fetch(`https://spotifymoods.herokuapp.com/mood/${mood}`).then(data=>data.json()).then(d=>{
-            d.results.forEach(r=>{
-            o = document.createElement("option")
-            o.text = r
-            pannel.add(o)
-  })
         })}
+
+// trying to re-write code since first one isn't working 
+
+// function populateTable(song) {
+//   var pannel = d3.select("#myInput");
+
+//   data.forEach(function(song) {
+//     var row = pannel.append("tr");
+//   Object.entries(song).forEach(value) {
+//     var cell = row.append("td");
+//     cell.text(value);
+//   };
+//  });
+// };
+
+// d3.json(`https://spotifymoodml.herokuapp.com/similar/${song}`).then((data) => {
+// console.log(data);
+// populateTable(data)
+// });
+
+// var button = d3.select("#myInput")
+
+// function PressSubmit () {
+
+//   d3.select("#exampleFormControlSelect2").addEventListener("change", populateTable(this.val))
+
+//   fetch(`https://spotifymoodml.herokuapp.com/similar/${song}`).then(data=>data.json()).then(d=>{
+//         console.log(data);
+
+// });  
